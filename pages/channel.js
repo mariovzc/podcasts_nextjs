d99a863e15cdc0bc106e5522dacd70007a854e88
mariovzc <mario.vizcainos@gmail.com>
2019-07-15 21:12:08 -0500

    display data using promises

--- pages/channel.js
+++ pages/channel.js
@@ -1,92 +1,142 @@
+import fetch from 'isomorphic-fetch';
+import Link from 'next/link';
+
 const Channel = ({ channel, audioClips, series }) => (
     <div>
         <header>PodCast</header>
-
+        <div 
+            className="banner"
+            style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }}
+        />
         <h1>{ channel.title }</h1>
 
 
-        <h2>Series</h2>
-        <div>
-            {series.map((serie) => (
-                <div> 
-                    { serie.title }
+        {series.length > 0 &&
+            <div>
+                <h2>Series</h2>
+                <div className="channels">
+                    {series.map((serie) => (
+                        <Link href={`/channel?id=${ serie.id }`} prefetch key={serie.id}>
+                            <a className="channel">
+                                <img src={ serie.urls.logo_image.original }
+                                     alt={serie.title}/>
+                                <h2>{ serie.title }</h2>
+                            </a>
+                        </Link>
+                    ))}
                 </div>
-            ))}
-        </div>
+            </div>
+        }
+        {audioClips.length > 0 &&
+            <div>
+                { audioClips.map(clip => (
+                    <Link href={`/podcast?id=${clip.id}`} prefetch key={clip.id}>
+                        <a className='podcast'>
+                            <h3>{ clip.title }</h3>
+                            <div className='meta'>
+                                { Math.ceil(clip.duration / 60) } minutes
+                            </div>
+                        </a>
+                    </Link>
+                )) }
+            </div>
+        }
 
-        <h2>Clips</h2>
-        <div>
-            {audioClips.map((clip) => (
-                <div> 
-                    { clip.title }
-                </div>
-            ))}
-        </div>
+        
 
         <style jsx>{`
-            header{
-                color: #fff;
-                background: #8757ca;
-                padding: 15px;
-                text-align: center;
-            }
-            .channels {
-                display: grid;
-                grid-gap: 15px;
-                padding: 15px;
-                grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
-            }
-            .channel {
-                display: block;
-                border-radius: 3px;
-                box-shadow: 02px 6px rgba(0,0,0,0.15);
-                margin-bottom: 0.5em;
-                border-width: 0.5px 2px 0px 2px;
-                border-color: rgba(0,0,0,0.15);
-                border-style: solid;
-                text-decoration: none;
-            }
-            .channel img{
-                width: 100%;
-            }
-            h1{
-                font-weight: 600;
-                padding: 15px;
-            }
-            h2{
-                padding: 5px;
-                font-size: 0.9em;
-                font-weight: 600;
-                margin: 0;
-                text-align:center;
-            }        
-        `}</style>
-        <style jsx global>{`
-            body{
-                margin:0;
-                font-family: system-ui;
-                background: white;
-            }
-        `}</style>
+        header {
+          color: #fff;
+          background: #8756ca;
+          padding: 15px;
+          text-align: center;
+        }
+
+        .banner {
+          width: 100%;
+          padding-bottom: 25%;
+          background-position: 50% 50%;
+          background-size: cover;
+          background-color: #aaa;
+        }
+
+        .channels {
+          display: grid;
+          grid-gap: 15px;
+          padding: 15px;
+          grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+        }
+        a.channel {
+          display: block;
+          margin-bottom: 0.5em;
+          color: #333;
+          text-decoration: none;
+        }
+        .channel img {
+          border-radius: 3px;
+          box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+          width: 100%;
+        }
+        h1 {
+          font-weight: 600;
+          padding: 15px;
+        }
+        h2 {
+          padding: 5px;
+          font-size: 0.9em;
+          font-weight: 600;
+          margin: 0;
+          text-align: center;
+        }
+
+        .podcast {
+          display: block;
+          text-decoration: none;
+          color: #333;
+          padding: 15px;
+          border-bottom: 1px solid rgba(0,0,0,0.2);
+          cursor: pointer;
+        }
+        .podcast:hover {
+          color: #000;
+        }
+        .podcast h3 {
+          margin: 0;
+        }
+        .podcast .meta {
+          color: #666;
+          margin-top: 0.5em;
+          font-size: 0.8em;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        body {
+          margin: 0;
+          font-family: system-ui;
+          background: white;
+        }
+      `}</style>
     </div>
 );
 
 Channel.getInitialProps = async ({ query }) => {
     const channel_id = query.id;
 
-    let reqChannel = await fetch(`
-        https://api.audioboom.com/channels/${channel_id}`);
-    let dataChannel = await reqChannel.json();
+    let [reqChannel,reqAudios, reqSeries ] = await Promise.all([
+        fetch(`https://api.audioboom.com/channels/${channel_id}`),
+        fetch(`https://api.audioboom.com/channels/${channel_id}/audio_clips`),
+        fetch(`https://api.audioboom.com/channels/${channel_id}/child_channels`)
+    ])
+
+    let [dataChannel, dataAudios, dataSeries] = await Promise.all([
+        reqChannel.json(),
+        reqAudios.json(),
+        reqSeries.json()
+    ])
+
     let channel = dataChannel.body.channel;
-
-    let reqAudios = await fetch(`
-        https://api.audioboom.com/channels/${channel_id}/audio_clips`);
-    let dataAudios = await reqAudios.json();
     let audioClips = dataAudios.body.audio_clips;
-
-    let reqSeries = await fetch(
-        `https://api.audioboom.com/channels/${channel_id}/child_channels`);
-    let dataSeries = await reqSeries.json();
     let series = dataSeries.body.channels;
 
 
