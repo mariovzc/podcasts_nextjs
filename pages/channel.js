73e5a83c6e8a06ed79bc6921371348f2823c263b
mariovzc <mario.vizcainos@gmail.com>
2019-07-16 21:57:52 -0500

    handle app errors

--- pages/channel.js
+++ pages/channel.js
@@ -1,65 +1,76 @@
 import fetch from 'isomorphic-fetch';
-import Link from 'next/link';
+import Error from 'next/error';
 import Layout from '../components/Layout';
 import ChannelGrid from '../components/ChannelGrid';
 import Podcasts from '../components/Podcasts';
 
-const Channel = ({ channel, audioClips, series }) => (
-    <Layout title={channel.title}>
+const Channel = ({ channel, audioClips, series, statusCode }) => {
+    if (statusCode != 200) {
+      return <Error statusCode={statusCode} />
+    }
+    return(
+      <Layout title={channel.title}>
         <div 
-            className="banner"
-            style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }}
+          className="banner"
+          style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }}
         />
         {series.length > 0 &&
           <div>
-              <h1>Series</h1>
-              <ChannelGrid channels={series} />
+            <h1>Series</h1>
+            <ChannelGrid channels={series} />
           </div>
         }
         {audioClips.length > 0 &&
           <Podcasts audioClips={audioClips} />
         }
-
-        
-
         <style jsx>{`
-        .banner {
-          width: 100%;
-          padding-bottom: 25%;
-          background-position: 50% 50%;
-          background-size: cover;
-          background-color: #aaa;
-        }
-        h1 {
-          font-weight: 600;
-          padding: 15px;
-        }
-      `}</style>
-    </Layout>
-);
+          .banner {
+            width: 100%;
+            padding-bottom: 25%;
+            background-position: 50% 50%;
+            background-size: cover;
+            background-color: #aaa;
+          }
+          h1 {
+            font-weight: 600;
+            padding: 15px;
+          }
+        `}</style>
+      </Layout>
+    )
+};
 
-Channel.getInitialProps = async ({ query }) => {
+Channel.getInitialProps = async ({ query, res }) => {
     const channel_id = query.id;
 
-    let [reqChannel,reqAudios, reqSeries ] = await Promise.all([
+    try{
+      let [reqChannel,reqAudios, reqSeries ] = await Promise.all([
         fetch(`https://api.audioboom.com/channels/${channel_id}`),
         fetch(`https://api.audioboom.com/channels/${channel_id}/audio_clips`),
         fetch(`https://api.audioboom.com/channels/${channel_id}/child_channels`)
-    ])
+      ])
 
-    let [dataChannel, dataAudios, dataSeries] = await Promise.all([
+      if (reqChannel.status >= 400) {
+        res.statusCode = reqChannel.status;
+        return {channel: null, audioClips: null, series: null, statusCode: reqChannel.status}  
+      }
+
+
+      let [dataChannel, dataAudios, dataSeries] = await Promise.all([
         reqChannel.json(),
         reqAudios.json(),
         reqSeries.json()
-    ])
+      ])
 
-    let channel = dataChannel.body.channel;
-    let audioClips = dataAudios.body.audio_clips;
-    let series = dataSeries.body.channels;
+      let channel = dataChannel.body.channel;
+      let audioClips = dataAudios.body.audio_clips;
+      let series = dataSeries.body.channels;
 
-
-    return { channel, audioClips, series }
-
+      return { channel, audioClips, series, statusCode: 200 }
+    } catch(e){
+      res.statusCode = 503;
+      return {channel: null, audioClips: null, series: null, statusCode: 503}
+    }
 }
 
 export default Channel;
