e852feef2f51242fd18692583473dbfc460abf47
mariovzc <mario.vizcainos@gmail.com>
2019-07-16 21:41:12 -0500

    handle server errors

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,30 @@
 import fetch from 'isomorphic-fetch';
+import  Error from 'next/error';
 import Layout from '../components/Layout';
 import ChannelGrid from '../components/ChannelGrid';
 
-const Index = ({ channels }) => {
-    return(
-        <Layout title="Podcasts">
-            <ChannelGrid channels={channels} />
-        </Layout>
-    );
+const Index = ({ channels, statusCode }) => {
+
+  if (statusCode != 200) {
+    return <Error statusCode={statusCode} />
+  }
+
+  return(
+    <Layout title="Podcasts">
+      <ChannelGrid channels={channels} />
+    </Layout>
+  );
 }
 
-Index.getInitialProps = async () => {
+Index.getInitialProps = async ({ res }) => {
+  try {
     let req = await fetch('https://api.audioboom.com/channels/recommended');
     let channels = await req.json();
-    return { channels: channels.body }
+    return { channels: channels.body, statusCode: 200 }  
+  } catch(e) {
+    res.statusCode = 503;
+    return { channels: null, statusCode: 503 }
+  }
 }
 
 export default Index;
